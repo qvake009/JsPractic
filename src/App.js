813f88997ec62b537d0472bd93fb88f1ba56c854
Your Name <you@example.com>
2021-07-03 16:29:12 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <div className="main">
+          <ul>
+            <li>VS Code Extensions</li>
+            <li>VS Code hot kays</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>БЭМ</li>
+            <li>GIT</li>
+            <li>BASH</li>
+            <li>JavaScript</li>
+            <li>RECT</li>
+            <li>Rdux</li>
+          </ul>
+        </div>
+      </header>
+      <div className="content">
+        <div className="page_name">
+          <h3>VS Code Extensions</h3>
+        </div>
+        <div className="content_items">
+          <ul>
+            <li><h5>npm</h5><span>(Node Package Manager) — менеджер пакетов, входящий в состав Node.js.</span></li>
+            <li><h5>Auto Rename Tag</h5><span> — автоматически переименовывать парный тег HTML </span></li>
+            <li><h5>Bracket Pair Colorizer</h5><span> — позволяет раскрашивать скобки, что облегчает нахождение пар открывающих и закрывающих скобок.</span></li>
+            <li><h5>Code Runner</h5><span> — проверяет код на наличие ошибок и распространенных проблем, подчеркивая ошибки и описывая проблему.</span></li>
+            <li><h5>ESLint</h5><span> — статически анализирует код, чтобы быстро находить проблемы. </span></li>
+            <li><h5>JavaScript (ES6) code snippets</h5><span> — Emmet для JS</span></li>
+            <li><h5>Live Server</h5><span> — локальный сервер с функцией перезагрузки в реальном времени для статических и динамических страниц.</span></li>
+            <li><h5>MIthril Emmet</h5></li>
+            <li><h5>open in browser</h5><span> — Alt + B, чтобы открыть текущий html-файл в браузере по умолчанию</span></li>
+            <li><h5>Prettier - Code formatter</h5></li>
+            <li><h5>Quokka.js</h5></li>
+            <li><h5>vscode-icons</h5></li>
+          </ul>
+            <div className="btn1">
+            <button className="btn"><a href="https://medium.com/@eshwaren/enable-emmet-support-for-jsx-in-visual-studio-code-react-f1f5dfe8809c">Enable Emmet support for JSX in Visual Studio Code | React </a></button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
